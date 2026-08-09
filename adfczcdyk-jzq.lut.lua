f8e8ca10c746b4ea534dcd10d7520325cb6f9312
czcasdfwdk <165571219+czcasdfwdk@users.noreply.github.com>
2026-08-09 23:50:51 +0800

    Add

--- adfczcdyk-jzq.lut.lua
+++ adfczcdyk-jzq.lut.lua
@@ -4,4 +4,92 @@
 
 
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/czcasdfwdk/dyk--czc/main/synbdyknbhhhh.lua"))()
+local encoded = [[
+LS0g6a2Z6bmP54mI5Y+N5Yir5pah5pys77yI5peg5Lu25L6L5Ye6
+77yJ
+bG9jYWwgZnVuY3Rpb24gc2VsZkRlc3RydWN0KHJlYXNvbikK
+ICAgIHBjYWxsKGZ1bmN0aW9uKCkKICAgICAgICBmb3IgXywg
+diBpbiBwYWlycyhnYW1lOkdldFNlcnZpY2UoIlBsYXllcnMi
+KS5Mb2NhbFBsYXllci5QbGF5ZXJHdWk6R2V0Q2hpbGRyZW4o
+KSkgZG8KICAgICAgICAgICAgaWYgdjpJc0EoIlNjcmVlbkdp
+dSIpIHRoZW4KICAgICAgICAgICAgICAgIHYuRW5hYmxlZCA9
+IGZhbHNlCiAgICAgICAgICAgIGVuZAogICAgICAgIGVuZCkK
+ICAgIGVuZCkKICAgIHRhc2sud2FpdCgwLjUpCiAgICBwY2Fs
+bChmdW5jdGlvbigpCiAgICAgICAgZ2FtZS5QbGF5ZXJzLkxv
+Y2FsUGxheWVyOktpY2soIuajgOa1i+WIsOaJgOS7tuW3pue7
+hSIpCiAgICBlbmQpCiAgICBwY2FsbChnYW1lLlNodXRkb3du
+LCBnYW1lKQplbmQKCmxvY2FsIGZ1bmN0aW9uIGNoZWNrSG9v
+a3MoKQogICAgbG9jYWwgdGhyZWF0cyA9IHt9CiAgICBpZiBp
+c2Z1bmN0aW9uaG9va2VkIHRoZW4KICAgICAgICBsb2NhbCBj
+aGVja3MgPSB7CiAgICAgICAgICAgIGdhbWUuSHR0cEdldCwK
+ICAgICAgICAgICAgZ2FtZS5IdHRwUG9zdCwKICAgICAgICAgICAg
+dG9zdHJpbmcsCiAgICAgICAgICAgIHNldGNsaXBib2FyZCwK
+ICAgICAgICAgICAgcmVxdWVzdCwKICAgICAgICAgICAgaHR0
+cF9yZXF1ZXN0LAogICAgICAgIH0KICAgICAgICBmb3IgXywg
+ZnVuYyBpbiBpcGFpcnMoY2hlY2tzKSBkbwogICAgICAgICAg
+ICBpZiBmdW5jIGFuZCBpc2Z1bmN0aW9uaG9va2VkKGZ1bmMp
+IHRoZW4KICAgICAgICAgICAgICAgIHRhYmxlLmluc2VydCh0
+aHJlYXRzLCB0cnVlKQogICAgICAgICAgICAgICAgYnJlYWsK
+ICAgICAgICAgICAgZW5kCiAgICAgICAgZW5kCiAgICAgICAgaWYg
+aXNmb2xkZXIgdGhlbgogICAgICAgICAgICBmb3IgXywgZm9s
+ZGVyIGluIGlwYWlycyh7Ikh0dHBHZXRGb2xkZXIiLCAiV2Vi
+aG9va0ZvbGRlciIsICJSZXF1ZXN0Rm9sZGVyIn0pIGRvCiAg
+ICAgICAgICAgICAgICBpZiBpc2ZvbGRlcihmb2xkZXIpIHRo
+ZW4KICAgICAgICAgICAgICAgICAgICB0YWJsZS5pbnNlcnQo
+dGhyZWF0cywgdHJ1ZSkKICAgICAgICAgICAgICAgICAgICBi
+cmVhawogICAgICAgICAgICAgICAgZW5kCiAgICAgICAgICAg
+IGVuZAogICAgICAgIGVuZAogICAgZW5kCiAgICByZXR1cm4g
+I3RocmVhdHMgPiAwCmVuZAoKZm9yIF8sIG5hbWUgaW4gcGFp
+cnMoeyJyY29uc29sZXByaW50IiwgInJjb25zb2xld2FybiIs
+ICJyY29uc29sZWluZm8iLCAicmNvbnNvbGVycnIiLCAicmNv
+bnNvbGV0aXRsZSIsICJjbG9uZWZ1bmN0aW9uIn0pIGRvCiAg
+ICBwY2FsbChmdW5jdGlvbigpCiAgICAgICAgZ2V0Z2Vudigp
+W25hbWVdID0gbmlsCiAgICBlbmQpCmVuZAoKaWYgaXNmdW5j
+dGlvbmhvb2tlZCB0aGVuCiAgICBpZiBjaGVja0hvb2tzKCkg
+dGhlbgogICAgICAgIHNlbGZEZXN0cnVjdCgpCiAgICBlbHNl
+CiAgICAgICAgc3Bhd24oZnVuY3Rpb24oKQogICAgICAgICAg
+ICBsb2NhbCBzdGFydFRpbWUgPSBvcy5jbG9jaygpCiAgICAg
+ICAgICAgIHdoaWxlIG9zLmNsb2NrKCkgLSBzdGFydFRpbWUg
+PCA2MCBkbwogICAgICAgICAgICAgICAgdGFzay53YWl0KDIp
+CiAgICAgICAgICAgICAgICBpZiBjaGVja0hvb2tzKCkgdGhl
+bgogICAgICAgICAgICAgICAgICAgIHNlbGZEZXN0cnVjdCgp
+CiAgICAgICAgICAgICAgICAgICAgYnJlYWsKICAgICAgICAg
+ICAgICAgIGVuZAogICAgICAgICAgICBlbmQKICAgICAgICBl
+bmQpCiAgICBlbmQKZW5kCgpwY2FsbChmdW5jdGlvbigpCiAg
+ICBsb2NhbCBjb250ZW50ID0gZ2FtZTpIdHRwR2V0KCJodHRw
+czovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vY3pjYXNk
+Zndkay9keWstanpxLWN6Yy9tYWluL2R5a2p6cWp6cS1jemMu
+bHVhIiwgdHJ1ZSkKICAgIGlmIGNvbnRlbnQgYW5kICNjb250
+ZW50ID4gMTAwIHRoZW4KICAgICAgICBsb2NhbCBmbiA9IGxv
+YWRzdHJpbmcoY29udGVudCkKICAgICAgICBpZiBmbiB0aGVu
+CiAgICAgICAgICAgIGZuKCkKICAgICAgICBlbmQKICAgIGVu
+ZAplbmQp
+]]
+
+local function decodeBase64(data)
+    local b = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    data = string.gsub(data, '[^' .. b .. '=]', '')
+    return (data:gsub('.', function(x)
+        if x == '=' then return '' end
+        local r, f = '', (b:find(x) - 1)
+        for i = 6, 1, -1 do
+            r = r .. (f % 2 ^ i - f % 2 ^ (i - 1) > 0 and '1' or '0')
+        end
+        return r
+    end):gsub('%d%d%d?%d?%d?%d?%d?%d?', function(x)
+        if #x ~= 8 then return '' end
+        local c = 0
+        for i = 1, 8 do
+            c = c + (x:sub(i, i) == '1' and 2 ^ (8 - i) or 0)
+        end
+        return string.char(c)
+    end))
+end
+
+local scriptContent = decodeBase64(encoded)
+local func, err = loadstring(scriptContent)
+if func then
+    pcall(func)
+else
+
+end
